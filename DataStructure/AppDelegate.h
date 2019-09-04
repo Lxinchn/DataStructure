8202feeea185c267524efb4f96ce0dfcefb97894
liuxin <liuxin.chn@foxmail.com>
2019-09-04 14:28:22 +0800

    Initial Commit

---
+++ DataStructure/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DataStructure
+//
+//  Created by GOGO on 2019/9/4.
+//  Copyright © 2019 Lxin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ DataStructure/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  DataStructure
+//
+//  Created by GOGO on 2019/9/4.
+//  Copyright © 2019 Lxin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
